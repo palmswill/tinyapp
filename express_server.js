ca12db3cec09f617338f804e408746d802cd5fcc
William Liu <liuwilly4@gmail.com>
2022-01-24 17:34:07 -0500

    added redirectable short url

--- express_server.js
+++ express_server.js
@@ -37,8 +37,10 @@ app.get("/urls", (req, res) => {
 
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const randomShortString=generateRandomString();
+  urlDatabase[randomShortString]=`http://${req.body.longURL}`;
+  console.log(`${randomShortString} : ${req.body.longURL}`);  // Log the POST request body to the console
+  res.redirect("/urls")         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
@@ -47,7 +49,8 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] /* What goes here? */ };
-  res.render("urls_show", templateVars);
+  res.redirect(urlDatabase[req.params.shortURL]);
+  // res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
